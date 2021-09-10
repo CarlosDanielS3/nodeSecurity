b84b8003ab0b0947e8f7a52ca50fff75198e1b7a
Carlos Daniel <gordonkoerich@gmail.com>
2021-09-10 14:22:42 -0300

    v2

--- src/usuarios/middlewares-autenticacao.js
+++ src/usuarios/middlewares-autenticacao.js
@@ -1,7 +1,10 @@
 const passport = require('passport');
+const Usuario = require('./usuarios-modelo');
+const { InvalidArgumentError } = require('../erros');
+const tokens = require('./tokens');
 
 module.exports = {
-  local: (req, res, next) => {
+  local(req, res, next) {
     passport.authenticate(
       'local',
       { session: false },
@@ -24,7 +27,7 @@ module.exports = {
     )(req, res, next);
   },
 
-  bearer: (req, res, next) => {
+  bearer(req, res, next) {
     passport.authenticate(
       'bearer',
       { session: false },
@@ -52,5 +55,43 @@ module.exports = {
         return next();
       }
     )(req, res, next);
-  }
+  },
+
+  async refresh(req, res, next) {
+    try {
+      const { refreshToken } = req.body;
+      const id = await tokens.refresh.verifica(refreshToken);
+      await tokens.refresh.invalida(refreshToken);
+      req.user = await Usuario.buscaPorId(id);
+      return next();
+    } catch (erro) {
+      if (erro.name === 'InvalidArgumentError') {
+        return res.status(401).json({ erro: erro.message });
+      }
+      return res.status(500).json({ erro: erro.message });
+    }
+  },
+
+  async verificacaoEmail(req, res, next) {
+    try {
+      const { token } = req.params;
+      const id = await tokens.verificacaoEmail.verifica(token);
+      const usuario = await Usuario.buscaPorId(id);
+      req.user = usuario;
+      next();
+    } catch (erro) {
+      if (erro.name === 'JsonWebTokenError') {
+        return res.status(401).json({ erro: erro.message });
+      }
+
+      if (erro.name === 'TokenExpiredError') {
+        return res.status(401).json({
+          erro: erro.message,
+          expiradoEm: erro.expiredAt,
+        });
+      }
+
+      return res.status(500).json({ erro: erro.message });
+    }
+  },
 };
